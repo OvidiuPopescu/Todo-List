0637ae7e70d1cd7383cbccb71a176597a0b58a98
OvidiuPopescu <108274522+OvidiuPopescu@users.noreply.github.com>
2022-06-27 15:25:37 +0300

    Delete src directory

--- src/loader.js
+++
@@ -1,8 +0,0 @@
-s = document.createElement("script");              /*variabila in care se afla un element script...*/
-s.src = chrome.runtime.getURL("src/youtubedl.js"); /*...a carui surs este acest fisier*/
-
-s.onload = function () {
-    this.remove();
-}
-
-document.head.appendChild(s); /*append s to header element of the page*/
